7fd12fbed63b6f0c9b7ce51f0ba9466f7936c42f
Jake Taylor <jake@elixxir.io>
2022-09-08 15:36:44 -0500

    fix headers

--- indexedDb/implementation.go
+++ indexedDb/implementation.go
@@ -1,7 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2022 Privategrity Corporation                                   /
-//                                                                             /
-// All rights reserved.                                                        /
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file                                                               //
 ////////////////////////////////////////////////////////////////////////////////
 
 //go:build js && wasm
