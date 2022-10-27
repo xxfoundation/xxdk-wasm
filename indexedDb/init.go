5a23b30148f5ae8f4dba767ad2e48bc121cb536a
benjamin <ben@elixxir.io>
2022-10-27 07:26:40 -0700

    removed database hacks

--- indexedDb/init.go
+++ indexedDb/init.go
@@ -108,22 +108,6 @@ func newWASMModel(databaseName string, encryption cryptoChannel.Cipher,
 		jww.WARN.Printf("IndexedDb encryption disabled!")
 	}
 
-	// Attempt to ensure the database has been properly initialized
-	if err != nil {
-		return nil, err
-	}
-	openRequest, err = idb.Global().Open(ctx, databaseName, currentVersion,
-		func(db *idb.Database, oldVersion, newVersion uint) error {
-			return nil
-		})
-	if err != nil {
-		return nil, err
-	}
-	// Wait for database open to finish
-	db, err = openRequest.Await(ctx)
-	if err != nil {
-		return nil, err
-	}
 	wrapper := &wasmModel{db: db, receivedMessageCB: cb, cipher: encryption}
 
 	return wrapper, nil
