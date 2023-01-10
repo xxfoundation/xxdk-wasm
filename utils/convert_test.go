37cfa2429456f7e0f2fe72e9106c880e0aa42c6b
Jono Wenger <jono@elixxir.io>
2023-01-10 10:38:04 -0800

    Add JsErrorToJson, which should be used to convert a Javascript error to a string

--- utils/convert_test.go
+++ utils/convert_test.go
@@ -241,3 +241,65 @@ func TestJsonToJSJsToJson(t *testing.T) {
 			"\nexpected: %s\nreceived: %s", jsonData, jsJson)
 	}
 }
+
+// Tests that JsErrorToJson can convert a Javascript object to JSON that matches
+// the output of json.Marshal on the Go version of the same object.
+func TestJsErrorToJson(t *testing.T) {
+	testObj := map[string]any{
+		"nil":    nil,
+		"bool":   true,
+		"int":    1,
+		"float":  1.5,
+		"string": "I am string",
+		"array":  []any{1, 2, 3},
+		"object": map[string]any{"int": 5},
+	}
+
+	expected, err := json.Marshal(testObj)
+	if err != nil {
+		t.Errorf("Failed to JSON marshal test object: %+v", err)
+	}
+
+	jsJson := JsErrorToJson(js.ValueOf(testObj))
+
+	// Javascript does not return the JSON object fields sorted so the letters
+	// of each Javascript string are sorted and compared
+	er := []rune(string(expected))
+	sort.SliceStable(er, func(i, j int) bool { return er[i] < er[j] })
+	jj := []rune(jsJson)
+	sort.SliceStable(jj, func(i, j int) bool { return jj[i] < jj[j] })
+
+	if string(er) != string(jj) {
+		t.Errorf("Recieved incorrect JSON from Javascript object."+
+			"\nexpected: %s\nreceived: %s", expected, jsJson)
+	}
+}
+
+// Tests that JsErrorToJson return a null object when the Javascript object is
+// undefined.
+func TestJsErrorToJson_Undefined(t *testing.T) {
+	expected, err := json.Marshal(nil)
+	if err != nil {
+		t.Errorf("Failed to JSON marshal test object: %+v", err)
+	}
+
+	jsJson := JsErrorToJson(js.Undefined())
+
+	if string(expected) != jsJson {
+		t.Errorf("Recieved incorrect JSON from Javascript object."+
+			"\nexpected: %s\nreceived: %s", expected, jsJson)
+	}
+}
+
+// Tests that JsErrorToJson returns a JSON object containing the original error
+// string.
+func TestJsErrorToJson_ErrorObject(t *testing.T) {
+	expected := "An error"
+	jsErr := Error.New(expected)
+	jsJson := JsErrorToJson(jsErr)
+
+	if !strings.Contains(jsJson, expected) {
+		t.Errorf("Recieved incorrect JSON from Javascript error."+
+			"\nexpected: %s\nreceived: %s", expected, jsJson)
+	}
+}
