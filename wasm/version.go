37b5f022d67dd3fff83f77cf095b49fc75683a3e
Jono Wenger <jono@elixxir.io>
2023-01-12 10:02:41 -0800

    Add GetWasmSemanticVersion and GetXXDKSemanticVersion

--- wasm/version.go
+++ wasm/version.go
@@ -10,8 +10,10 @@
 package wasm
 
 import (
+	"encoding/json"
 	"gitlab.com/elixxir/client/v4/bindings"
 	"gitlab.com/elixxir/xxdk-wasm/storage"
+	"gitlab.com/elixxir/xxdk-wasm/utils"
 	"syscall/js"
 )
 
@@ -48,4 +50,61 @@ func GetClientGitVersion(js.Value, []js.Value) any {
 //   - Dependency list (string).
 func GetClientDependencies(js.Value, []js.Value) any {
 	return bindings.GetDependencies()
+}
+
+// VersionInfo contains information about the current and old version of the
+// API.
+type VersionInfo struct {
+	Current string `json:"current"`
+	Updated bool   `json:"updated"`
+	Old     string `json:"old"`
+}
+
+// GetWasmSemanticVersion returns the current version of the WASM client, it's
+// old version before being updated, and if it has been updated.
+//
+// Returns:
+//   - JSON of [VersionInfo] (Uint8Array).
+//   - Throws a TypeError if getting the version failed.
+func GetWasmSemanticVersion(js.Value, []js.Value) any {
+	vi := VersionInfo{
+		Current: storage.SEMVER,
+		Updated: true,
+		Old:     storage.GetOldWasmSemVersion(),
+	}
+
+	if vi.Current == vi.Old {
+		vi.Updated = false
+	}
+
+	data, err := json.Marshal(vi)
+	if err != nil {
+		utils.Throw(utils.TypeError, err)
+	}
+
+	return utils.CopyBytesToJS(data)
+}
+
+// GetXXDKSemanticVersion returns the current version of the xxdk client, it's
+// old version before being updated, and if it has been updated.
+//
+// Returns:
+//   - JSON of [VersionInfo] (Uint8Array).
+//   - Throws a TypeError if getting the version failed.
+func GetXXDKSemanticVersion(js.Value, []js.Value) any {
+	vi := VersionInfo{
+		Current: bindings.GetVersion(),
+		Updated: true,
+		Old:     storage.GetOldClientSemVersion(),
+	}
+	if vi.Current == vi.Old {
+		vi.Updated = false
+	}
+
+	data, err := json.Marshal(vi)
+	if err != nil {
+		utils.Throw(utils.TypeError, err)
+	}
+
+	return utils.CopyBytesToJS(data)
 }
