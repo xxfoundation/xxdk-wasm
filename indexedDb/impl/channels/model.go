76728af64cb1c68768bdcb0edb62224f6677e252
Jake Taylor <jake@privategrity.com>
2023-03-16 17:28:59 +0000

    made several changes in an attempt for solidify behavior of receiveHelper

--- indexedDb/impl/channels/model.go
+++ indexedDb/impl/channels/model.go
@@ -45,7 +45,7 @@ const (
 // The user's nickname can change each message, but the rest does not. We
 // still duplicate all of it for each entry to simplify code for now.
 type Message struct {
-	ID              uint64    `json:"id"` // Matches pkeyName
+	ID              uint64    `json:"id,omitempty"` // Matches pkeyName
 	Nickname        string    `json:"nickname"`
 	MessageID       []byte    `json:"message_id"`        // Index
 	ChannelID       []byte    `json:"channel_id"`        // Index
