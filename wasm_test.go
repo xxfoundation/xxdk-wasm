676e9bdedbfb363a8baf4b052a75802a34871c36
Jake Taylor <jtaylorapps@gmail.com>
2023-04-04 16:50:28 -0500

    fixes for new client bindings

--- wasm_test.go
+++ wasm_test.go
@@ -58,6 +58,7 @@ func TestPublicFunctions(t *testing.T) {
 		// Mobile-specific bindings not supported by the browser
 		"NewChannelsManagerMobile":  {},
 		"LoadChannelsManagerMobile": {},
+		"NewDmManagerMobile":        {},
 
 		// C-Library specific bindings not needed by the browser
 		"GetDMInstance":   {},
